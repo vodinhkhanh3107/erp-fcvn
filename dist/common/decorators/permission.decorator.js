0b326117428b558d29ea0fe44d16f770968e79b0
Vo Dinh Khanh <vodinhkhanh3107>
2026-09-11 11:31:30 +0700

    chore: ignore dist folder

--- dist/common/decorators/permission.decorator.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RequirePermission = exports.PERMISSION_KEY = void 0;
-const common_1 = require("@nestjs/common");
-exports.PERMISSION_KEY = 'permission';
-const RequirePermission = (...permissions) => (0, common_1.SetMetadata)(exports.PERMISSION_KEY, permissions);
-exports.RequirePermission = RequirePermission;
-//# sourceMappingURL=permission.decorator.js.map
